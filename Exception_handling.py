76dd41f470896909a7593baa2e376257fd539dcf
HRK <admin@Haris-Macbook-pro.local>
2026-05-11 11:46:44 +0500

    first commit

---
+++ Exception_handling.py
@@ -0,0 +1,35 @@
+#Example 1
+name = 'haris'
+try:
+    name = int(name)
+except:
+    name = 1
+print("hello",name)
+
+#Example 2
+x = '345'
+y = "hello"
+try:
+    x = int(x)
+except:
+    y = int(y)
+print(x,y)
+
+
+#Example 3
+number = input("Enter a Number")
+try:
+    num = int(number)
+except:
+    num = -1
+if num > 0:
+    print("Great work")
+else:
+    print("input number")
+
+#Example 4
+try:
+    x = float(input("Enter your cgpa:"))
+    print("your Cgpa is =",x)
+except ValueError:
+    print("Please Enter your CGPA in numbers ....!")
